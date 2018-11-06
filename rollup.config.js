9021c1c9c12ed0c55d23ac18f38b534722a65da9
Sébastien Lombard <sebastien.lombard75@gmail.com>
2018-11-06 17:59:40 +0100

    fix(build): Fix build SCSS (#48)
    * ci(slack): Add Slack notification
    
    * ci(slack): Update slack notify
    
    * ci(slack): Add error handling
    
    * fix(config): Add type of release according to commit type (#43)
    
    * docs(styleguide.config.js): Add Aliases and sections on syleguidist (#44)
    
    * docs(nav): Update navbar (#42)
    
    * docs(nav): Update navbar
    
    * fix(scss): Fix typo index.scss
    
    * build(commitizen): update rules (#45)
    
    * build(lint): Add Eslint (#46)
    
    * docs(nav): Update navbar
    
    * fix(scss): Fix typo index.scss
    
    * add eslint
    
    * fix(build): Fix build SCSS

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,10 @@ import buble from 'rollup-plugin-buble'
 import { uglify } from 'rollup-plugin-uglify'
 import node from 'rollup-plugin-node-resolve'
 import vue from 'rollup-plugin-vue'
-import sass from 'rollup-plugin-sass'
+import scss from 'rollup-plugin-scss'
 import postcss from 'postcss'
 import autoprefixer from 'autoprefixer'
+import { eslint } from 'rollup-plugin-eslint'
 
 export default {
   input: 'src/index.js',
@@ -14,9 +15,15 @@ export default {
     name: 'agorizeUikit'
   },
   plugins: [
+    eslint({
+      exclude: [
+        'node_modules/**',
+        'src/styles/**'
+      ]
+    }),
     vue(),
     node(),
-    sass({
+    scss({
       output: true,
       processor: css => postcss([autoprefixer])
         .process(css)
